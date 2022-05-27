03999065fa7399f83c6f8ce1bbb230f168de5dcc
Omar Mohamed <mourra24@gmail.com>
2022-05-27 23:45:04 +0200

    added more GUI Screens
    added GUI screens and Giphys

--- demo/src/main/java/com/example/demo/LoginPage.java
+++ demo/src/main/java/com/example/demo/LoginPage.java
@@ -25,5 +25,8 @@ public class LoginPage {
     public void GotoSignUpPage(ActionEvent actionEvent) {
         System.out.println("ana hena");
     }
+
+    public void SignInTheUser(ActionEvent actionEvent) {
+    }
 }
 
