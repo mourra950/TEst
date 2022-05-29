b2858f0bd8da502164bf2a399a4f6486187ebcfe
Omar Mohamed <mourra24@gmail.com>
2022-05-29 21:34:19 +0200

    added Sql Commands to be able to edit the Data Base

--- demo/src/main/java/com/example/demo/TeacherPage.java
+++ demo/src/main/java/com/example/demo/TeacherPage.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class TeacherPage {
 
@@ -23,6 +24,9 @@ public class TeacherPage {
     private Label CourseName;
 
     @FXML
+    private TextArea DesField;
+
+    @FXML
     private Label DescriptionLabel;
 
     @FXML
@@ -35,6 +39,9 @@ public class TeacherPage {
     private Button L111;
 
     @FXML
+    private TextField L1field;
+
+    @FXML
     private Button L2;
 
     @FXML
@@ -44,6 +51,9 @@ public class TeacherPage {
     private Button L211;
 
     @FXML
+    private TextField L2field;
+
+    @FXML
     private Button L3;
 
     @FXML
@@ -51,6 +61,27 @@ public class TeacherPage {
 
     @FXML
     private Button L311;
+
+    @FXML
+    private TextField L3field;
+
+    @FXML
+    private TextField Q1field;
+
+    @FXML
+    private TextField Q2field;
+
+    @FXML
+    private TextField Q3field;
+
+    @FXML
+    private TextField S1field;
+
+    @FXML
+    private TextField S2field;
+
+    @FXML
+    private TextField S3field;
 
     @FXML
     void BackToStudentPage(ActionEvent event) {
@@ -64,53 +95,55 @@ public class TeacherPage {
 
     @FXML
     void UploadL1(ActionEvent event) {
-        String sql="UPDATE 'main'.'lectures' SET 'L1' ="+"''"+" WHERE (`Name` == '" + "DB" + "');";
 
     }
 
     @FXML
     void UploadL2(ActionEvent event) {
-        String sql="UPDATE 'main'.'lectures' SET 'L1' ="+"''"+" WHERE (`Name` == '" + "DB" + "');";
+
     }
 
     @FXML
     void UploadL3(ActionEvent event) {
-        String sql="UPDATE 'main'.'lectures' SET 'L1' ="+"''"+" WHERE (`Name` == '" + "DB" + "');";
+
     }
 
     @FXML
     void UploadQ1(ActionEvent event) {
-        String sql="UPDATE 'main'.'lectures' SET 'L1' ="+"''"+" WHERE (`Name` == '" + "DB" + "');";
+
     }
 
     @FXML
     void UploadQ2(ActionEvent event) {
-        String sql="UPDATE 'main'.'lectures' SET 'L1' ="+"''"+" WHERE (`Name` == '" + "DB" + "');";
+
     }
 
     @FXML
     void UploadQ3(ActionEvent event) {
-        String sql="UPDATE 'main'.'lectures' SET 'L1' ="+"''"+" WHERE (`Name` == '" + "DB" + "');";
+
     }
 
     @FXML
     void UploadS1(ActionEvent event) {
-        String sql="UPDATE 'main'.'lectures' SET 'L1' ="+"''"+" WHERE (`Name` == '" + "DB" + "');";
+
     }
 
     @FXML
     void UploadS2(ActionEvent event) {
-        String sql="UPDATE 'main'.'lectures' SET 'L1' ="+"''"+" WHERE (`Name` == '" + "DB" + "');";
+
     }
 
     @FXML
     void UploadS3(ActionEvent event) {
-        String sql="UPDATE 'main'.'lectures' SET 'L1' ="+"''"+" WHERE (`Name` == '" + "DB" + "');";
+        String sql="UPDATE 'main'.'lectures' SET 'S3' ="+S3field.getText()+" WHERE (`Name` == '" + LoginPage.CourseName + "');";
     }
 
     @FXML
     void initialize() {
-       
+
+
     }
 
 }
+
+
