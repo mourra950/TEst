7dad84c96223451ce20f80eb6e47ef16d41a3dbb
Omar Mohamed <mourra24@gmail.com>
2022-05-31 12:04:22 +0200

    added some scene transitions

--- demo/src/main/java/com/example/demo/CoursesStudent.java
+++ demo/src/main/java/com/example/demo/CoursesStudent.java
@@ -1,8 +1,17 @@
 package com.example.demo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class CoursesStudent {
 
@@ -25,20 +34,102 @@ public class CoursesStudent {
     private Button L3;
 
     @FXML
-    void BackToStudentPage(ActionEvent event) {
-
+    void BackToStudentPage(ActionEvent event) throws IOException {
+        LoginPage.CourseName=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(SignupPage.class.getResource("StudentPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) L3.getScene().getWindow();
+        stage.setTitle("Student");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void BackTomain(ActionEvent event) {
-
+    void BackTomain(ActionEvent event) throws IOException {
+        LoginPage.CourseName=null;
+        FXMLLoader fxmlLoader = new FXMLLoader(SignupPage.class.getResource("LoginPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) L3.getScene().getWindow();
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
     }
+    @FXML
+    void OA1(ActionEvent event) throws Exception {
+        String link= getLink("Assignments","A1");
+        Browser.open(link);
+    }
+
+    @FXML
+    void OA2(ActionEvent event) throws Exception {
+        String link=getLink("Assignments","A2");
+        Browser.open(link);
+    }
+
+    @FXML
+    void OA3(ActionEvent event) throws Exception {
+        String link=getLink("Assignments","A3");
+        Browser.open(link);
+    }
+
+    @FXML
+    void OL1(ActionEvent event) throws Exception {
+        String link=getLink("lectures","L1");
+        Browser.open(link);
+    }
+
+    @FXML
+    void OL2(ActionEvent event) throws Exception {
+        String link=getLink("lectures","L2");
+        Browser.open(link);
+    }
+
+    @FXML
+    void OL3(ActionEvent event) throws Exception {
+        String link=getLink("lectures","L3");
+        Browser.open(link);
+    }
+
+    @FXML
+    void OQ1(ActionEvent event) throws Exception {
+        String link=getLink("Quizes","Q1");
+        Browser.open(link);
+    }
+
+    @FXML
+    void OQ2(ActionEvent event) throws Exception {
+        String link=getLink("Quizes","Q2");
+        Browser.open(link);
+    }
+
+    @FXML
+    void OQ3(ActionEvent event) throws Exception {
+        String link=getLink("Quizes","Q3");
+        Browser.open(link);
+    }
+
     @FXML
     void initialize() {
         CourseCode.setText(StudentPage.courseCode);
         CourseName.setText(StudentPage.courseName);
         DescriptionLabel.setText(StudentPage.description);
 
+    }
+    public String getLink(String b, String c) throws SQLException, ClassNotFoundException {
+        //b anhy table , c anhy column
+        Connection con = DBconnector.connect();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String temp = null;
+        String sql = "SELECT * FROM 'main'.'" + b + "' WHERE Name = '" + LoginPage.CourseName + "'";
+        ps = con.prepareStatement(sql);
+        rs = ps.executeQuery();
+        while (rs.next()) {
+            temp = rs.getString(c);
+
+        }
+        System.out.println(temp);
+        return temp;
     }
 
 }
